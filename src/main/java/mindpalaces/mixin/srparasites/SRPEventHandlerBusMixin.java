ded549d669be5fd86a5ea0c175d5efc41ff80663
Nischhelm <nischhelm@gmail.com>
2025-12-12 00:12:41 +0100

    fix srp point compat mixin
    im pretty sure it worked already but still making sure

--- src/main/java/mindpalaces/mixin/srparasites/SRPEventHandlerBusMixin.java
+++ src/main/java/mindpalaces/mixin/srparasites/SRPEventHandlerBusMixin.java
@@ -3,6 +3,8 @@ package mindpalaces.mixin.srparasites;
 import com.dhanantry.scapeandrunparasites.util.handlers.SRPEventHandlerBus;
 import com.dhanantry.scapeandrunparasites.world.SRPSaveData;
 import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import mindpalaces.util.FromMPTeleporterThreadLocal;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
@@ -10,12 +12,14 @@ import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(SRPEventHandlerBus.class)
 public class SRPEventHandlerBusMixin {
-    @WrapWithCondition(
+    @WrapOperation(
             method = "playerUp",
             at = @At(value = "INVOKE", target = "Lcom/dhanantry/scapeandrunparasites/world/SRPSaveData;setTotalKills(IIZLnet/minecraft/world/World;Z)Z"),
             remap = false
     )
-    private boolean mindpalaces_dontIncreasePoints(SRPSaveData instance, int id, int in, boolean plus, World worldIn, boolean canChangePhase){
-        return !FromMPTeleporterThreadLocal.get(); //dont increase points if from teleporting to/from MP
+    private boolean mindpalaces_dontIncreasePoints(SRPSaveData instance, int id, int in, boolean plus, World worldIn, boolean canChangePhase, Operation<Boolean> original){
+        if(!FromMPTeleporterThreadLocal.get())
+            return original.call(instance, id, in, plus, worldIn, canChangePhase);
+        return false; //dont increase points if from teleporting to/from MP
     }
 }
